941667392d97a3fa344fa6a82dff031c48d10f47
Susmoy Barman <susmoy.barman.bd@gmail.com>
2019-02-13 12:46:52 +0600

    Delete Calculating_Function.java

--- Regular Falsi Method/Calculating_Function.java
+++
@@ -1,9 +0,0 @@
-package regular_falsi_method_root_finding;
-
-public class Calculating_Function {
-	// is x^3 - x^2 + 2 
-    double func(double x) 
-    { 
-        return (x * x * x - x * x + 2); 
-    } 
-}
