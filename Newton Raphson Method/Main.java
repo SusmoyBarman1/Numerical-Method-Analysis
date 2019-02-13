eca76561fefc070f63723a12542f2a082c26816f
Susmoy Barman <susmoy.barman.bd@gmail.com>
2019-02-13 12:42:08 +0600

    Newton Raphson Method

---
+++ Newton Raphson Method/Main.java
@@ -0,0 +1,12 @@
+package newton_raphson_method;
+
+public class Main {
+	  // Driver code 
+	public static void main (String[] args) 
+    { 
+          
+        // Initial values assumed 
+        double x0 = -20;  
+        NewtonRaphson.newtonRaphson(x0); 
+    }
+}
